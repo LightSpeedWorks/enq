9668448b705118b3f5e1596c1d728d333aed7aac
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-11-14 14:45:07 +0900

    examples

---
+++ examples/enq-ex.js
@@ -0,0 +1,12 @@
+var Queue = require('../enq');
+
+var q = new Queue();
+
+q.push('elem1');
+q.push('elem2');
+q.push('elem3');
+
+console.log(q.shift()); // -> 'elem1'
+console.log(q.shift()); // -> 'elem2'
+console.log(q.shift()); // -> 'elem3'
+console.log(q.shift()); // -> null
